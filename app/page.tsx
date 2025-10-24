430c8b3d66b5620c30879bcd8375f25f507b2570
Cedric <cedricctf11a@gmail.com>
2025-10-25 07:55:16 +0800

    fixed file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { WalletConnect } from './components/WalletConnect';
+import { WalletConnect } from '../components/WalletConnect';
 import Link from 'next/link';
 
 export default function Home() {
